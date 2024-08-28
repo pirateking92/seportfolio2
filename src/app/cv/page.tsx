cbba2cfdcbb378d6df63c89d4da358509eb2fc14
Matt Doyle <doyle9214@gmail.com>
2024-08-28 16:08:22 +0100

    added navbar to cv page. need to make this better

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -20,6 +20,7 @@ export default async function CVPage() {
 
   return (
     <div className="flex flex-col items-center pt-16 md:pt-20">
+      <Navbar />
       <h1 className="py-5 text-4xl text-slate-300 font-bold mb-4 text-center">
         Sepy Baghaei&apos;s CV
       </h1>
