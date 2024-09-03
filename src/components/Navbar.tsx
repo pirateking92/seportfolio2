45b1bd7f2b9a759b70ef3b772987d18df35f0094
Matt Doyle <doyle9214@gmail.com>
2024-09-03 22:14:10 +0100

    navbar change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -62,8 +62,11 @@ const Navbar: React.FC = () => {
   }, []);
 
   return (
-    <nav ref={navbarRef} className="fixed top-0 left-0 right-0 z-10 bg-current">
-      <div className="flex container lg:py-4 items-center justify-between mx-auto px-4 py-2">
+    <nav
+      ref={navbarRef}
+      className="fixed top-0 left-0 right-0 z-10 bg-black bg-opacity-100"
+    >
+      <div className="flex container lg:py-4 items-center justify-between mx-auto py-2">
         {/* Logo */}
         <h1>
           <Link href={"/"} className="text-xl md:text-3xl text-white">
