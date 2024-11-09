559394bfbbef4068fcf80342caed4eec856121d1
Matt Doyle <doyle9214@gmail.com>
2024-11-09 21:48:26 +0000

    final edit for the night, saw that navbar should be transparent, makes life easier

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -88,7 +88,7 @@ const Navbar: React.FC = () => {
   return (
     <nav
       ref={navbarRef}
-      className="fixed top-0 left-0 right-0 z-20 bg-black bg-opacity-90"
+      className="fixed top-0 left-0 right-0 z-20 bg-black bg-opacity-0"
     >
       <div className="flex container items-center justify-between mx-auto p-4">
         {/* Logo */}
