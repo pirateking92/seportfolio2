44d66b7d034972cdbcf539cef0228051b0cd1621
Matt Doyle <doyle9214@gmail.com>
2024-12-19 12:03:35 +0000

    speed insights added for vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "/src/app/globals.css";
 import "flowbite/dist/flowbite.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights";
 
 const headingFont = Indie_Flower({
   subsets: ["latin"],
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body className="bg-black-to-navy flex flex-col min-h-screen bg-no-repeat bg-fixed">
         <main className="flex-grow">{children}</main>
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   );
