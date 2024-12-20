6ef8b6293e3fe2ddf60da0678fe7623e99c88aca
Matt Doyle <doyle9214@gmail.com>
2024-12-20 15:11:50 +0000

    made a boo boo and mixed up pages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,23 @@
-import CVPage from "@/components/CVPage";
+import Contact from "@/components/Contact";
+import Navbar from "@/components/Navbar";
+import SmokeFadeIn from "@/components/SmokeFadeIn";
+import Image from "next/image";
 
-export default function CVPAGE() {
-  return <CVPAGE />;
+export default async function ContactPage() {
+  return (
+    <div>
+      <main className="flex-grow">
+        <SmokeFadeIn visibleOnLoad={true}>
+          <Image
+            src="/SepySits.jpg"
+            alt="Sepy Directing"
+            width={600}
+            height={400}
+            className="mx-auto m-8 pt-10"
+          />
+        </SmokeFadeIn>
+        <Contact />
+      </main>
+    </div>
+  );
 }
--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,47 +1,5 @@
-"use client";
+import CVPage from "@/components/CVPage";
 
-import { GET_CV_PAGE } from "../../lib/queries";
-import client from "../../../apollo-client";
-import Navbar from "@/components/Navbar";
-import { useEffect, useState } from "react";
-import SmokeFadeIn from "@/components/SmokeFadeIn";
-
-export default function CVPage() {
-  // have to set the types of two variables now for the useState
-  const [cvData, setCvData] = useState<{
-    title: string;
-    content: string;
-  } | null>(null);
-
-  useEffect(() => {
-    async function fetchCVData() {
-      const { data } = await client.query({
-        query: GET_CV_PAGE,
-      });
-      // within this part, we can have multiple variable for the multiple queries
-      setCvData({
-        title: data.page.title || "Untitled",
-        content: data.page.content || "No content found",
-      });
-    }
-    fetchCVData();
-  }, []);
-
-  return (
-    <div className="flex flex-col min-h-screen">
-      <SmokeFadeIn visibleOnLoad={false}>
-        <div className="flex-grow pt-24 m-5 mb-6">
-          {" "}
-          {/* Moved padding here */}
-          <div className="p-9 text-3xl text-slate-300 font-bold mb-4 text-left">
-            {cvData?.title}
-          </div>
-          <div
-            className="prose-lg p-9 text-slate-300"
-            dangerouslySetInnerHTML={{ __html: cvData?.content }}
-          />
-        </div>
-      </SmokeFadeIn>
-    </div>
-  );
+export default function CV() {
+  return <CVPage />;
 }
