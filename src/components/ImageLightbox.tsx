b80edc8404fdf86a05304028446a22cdf49cd770
Matt Doyle <doyle9214@gmail.com>
2025-11-29 11:13:51 +0000

    removed hover

--- src/components/ImageLightbox.tsx
+++ src/components/ImageLightbox.tsx
@@ -17,42 +17,11 @@ export default function ImageLightbox({
   children,
 }: ImageLightboxProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const [showPreview, setShowPreview] = useState(false);
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     setMounted(true);
   }, []);
-
-  const handleMouseMove = (e: React.MouseEvent) => {
-    setMousePosition({ x: e.clientX, y: e.clientY });
-  };
-
-  const previewContent = showPreview && (
-    <div
-      className="fixed pointer-events-none z-[9999]"
-      style={{
-        left:
-          mousePosition.x + 30 + 700 > window.innerWidth
-            ? mousePosition.x - 530
-            : mousePosition.x + 30,
-        top:
-          mousePosition.y - 250 < 0
-            ? mousePosition.y + 30
-            : mousePosition.y - 250,
-      }}
-    >
-      <div className="relative w-[700px] h-[700px] bg-black/95 rounded-lg overflow-hidden shadow-2xl border-4 border-white/30">
-        <Image
-          src={imageUrl}
-          alt={`Preview ${index + 1}`}
-          fill
-          className="object-contain p-2"
-        />
-      </div>
-    </div>
-  );
 
   const lightboxContent = isOpen && (
     <div
@@ -79,16 +48,8 @@ export default function ImageLightbox({
 
   return (
     <>
-      <div
-        onClick={() => setIsOpen(true)}
-        onMouseEnter={() => setShowPreview(true)}
-        onMouseLeave={() => setShowPreview(false)}
-        onMouseMove={handleMouseMove}
-      >
-        {children}
-      </div>
+      <div onClick={() => setIsOpen(true)}>{children}</div>
 
-      {mounted && createPortal(previewContent, document.body)}
       {mounted && createPortal(lightboxContent, document.body)}
     </>
   );
